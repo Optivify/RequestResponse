a43e4ea10427c81b91f312a1680e9f200fcc53f7
Optivify <optivify@gmail.com>
2023-06-28 20:21:38 +0700

    Add the list input interface

--- src/Optivify.RequestResponse/Requests/ListInput.cs
+++ src/Optivify.RequestResponse/Requests/ListInput.cs
@@ -1,46 +1,62 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace Optivify.RequestResponse
+namespace Optivify.RequestResponse.Requests;
+
+public interface IListInput
 {
-    public class ListInput
+    string? SearchText { get; }
+
+    int Page { get; }
+
+    int ItemsPerPage { get; }
+
+    string? SortBy { get; }
+
+    bool SortDesc { get; }
+
+    int Skip { get; }
+
+    int Take { get; }
+}
+
+public class ListInput : IListInput
+{
+    public virtual string? SearchText { get; set; }
+
+    public virtual int Page { get; set; }
+
+    public virtual int ItemsPerPage { get; set; }
+
+    public virtual string? SortBy { get; set; }
+
+    public virtual bool SortDesc { get; set; }
+
+    public virtual bool PaginationEnabled { get; set; }
+
+    [NotMapped]
+    [JsonIgnore]
+    public virtual int Skip
     {
-        public virtual string? SearchText { get; set; }
+        get { return this.ItemsPerPage * (this.Page - 1); }
+    }
 
-        public virtual int Page { get; set; }
+    [NotMapped]
+    [JsonIgnore]
+    public virtual int Take
+    {
+        get { return this.ItemsPerPage; }
+    }
 
-        public virtual int ItemsPerPage { get; set; }
+    public ListInput()
+    {
+        this.SetDefault();
+    }
 
-        public virtual string? SortBy { get; set; }
-
-        public virtual bool SortDesc { get; set; }
-
-        public virtual bool PaginationEnabled { get; set; }
-
-        [NotMapped]
-        [JsonIgnore]
-        public virtual int Skip
-        {
-            get { return this.ItemsPerPage * (this.Page - 1); }
-        }
-
-        [NotMapped]
-        [JsonIgnore]
-        public virtual int Take
-        {
-            get { return this.ItemsPerPage; }
-        }
-
-        public ListInput()
-        {
-            this.SetDefault();
-        }
-
-        protected void SetDefault()
-        {
-            this.Page = 1;
-            this.ItemsPerPage = 10;
-            this.PaginationEnabled = true;
-        }
+    protected void SetDefault()
+    {
+        this.Page = 1;
+        this.ItemsPerPage = 10;
+        this.PaginationEnabled = true;
     }
 }
