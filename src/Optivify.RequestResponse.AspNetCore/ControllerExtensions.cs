45b7c0903c6c321cb2a0c89fc54663ed2807ed4c
Optivify <optiviyf@gmail.com>
2023-07-13 22:52:04 +0700

    Add controller extensions

---
+++ src/Optivify.RequestResponse.AspNetCore/ControllerExtensions.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Optivify.RequestResponse.Responses;
+using Optivify.ServiceResult;
+
+namespace Optivify.RequestResponse.AspNetCore;
+
+public static class ControllerExtensions
+{
+    public static ActionResult ToActionResult<T>(this ControllerBase controller, Result<T> result)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Success => Success(controller, result),
+            ResultStatus.Error => Error(controller, result),
+            ResultStatus.Invalid => BadRequest(controller, result),
+            ResultStatus.NotFound => NotFound(controller, result),
+            ResultStatus.Unauthorized => Unauthorized(controller, result),
+            ResultStatus.Forbidden => Forbidden(controller),
+            _ => throw new NotSupportedException("The result status value is not supported."),
+        };
+    }
+
+    private static ResultResponse<T> CreateResultDataResponse<T>(Result<T> result)
+    {
+        return new ResultResponse<T>
+        {
+            Data = result.Value,
+            IsSuccess = result.IsSuccess,
+            ValidationErrors = result.ValidationErrors
+        };
+    }
+
+    private static IValidationResponse? GetValidationResultResponse<T>(Result<T> result)
+    {
+        if (result.Value is IValidationResponse response)
+        {
+            response.IsSuccess = result.IsSuccess;
+            response.ValidationErrors = result.ValidationErrors;
+
+            return response;
+        }
+
+        return null;
+    }
+
+    private static ActionResult Success<T>(ControllerBase controller, Result<T> result)
+    {
+        var validationResultResponse = GetValidationResultResponse(result);
+
+        if (validationResultResponse is null)
+        {
+            return controller.Ok(result.Value);
+        }
+
+        return controller.Ok(validationResultResponse);
+    }
+
+    private static ActionResult Error<T>(ControllerBase controller, Result<T> result)
+    {
+        var validationResultResponse = GetValidationResultResponse(result);
+
+        if (validationResultResponse is null)
+        {
+            return controller.UnprocessableEntity(CreateResultDataResponse(result));
+        }
+
+        return controller.UnprocessableEntity(validationResultResponse);
+    }
+
+    private static ActionResult BadRequest<T>(ControllerBase controller, Result<T> result)
+    {
+        var validationResultResponse = GetValidationResultResponse(result);
+
+        if (validationResultResponse is null)
+        {
+            return controller.BadRequest(CreateResultDataResponse(result));
+        }
+
+        return controller.BadRequest(validationResultResponse);
+    }
+
+    private static ActionResult NotFound<T>(ControllerBase controller, Result<T> result)
+    {
+        return controller.NotFound(result.CombineErrorMessages());
+    }
+
+    private static ActionResult Unauthorized<T>(ControllerBase controller, Result<T> result)
+    {
+        return controller.Unauthorized(result.CombineErrorMessages());
+    }
+
+    private static ActionResult Forbidden(ControllerBase controller)
+    {
+        return controller.Forbid();
+    }
+}
---
+++ src/Optivify.RequestResponse.AspNetCore/ResultExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Optivify.ServiceResult;
+using System.Text;
+
+namespace Optivify.RequestResponse.AspNetCore;
+
+public static class ResultExtensions
+{
+    public static string CombineErrorMessages(this IResult result)
+    {
+        var stringBuilder = new StringBuilder();
+
+        foreach (string errorMessage in result.ErrorMessages)
+        {
+            stringBuilder.AppendLine(errorMessage);
+
+            if (!errorMessage.EndsWith("."))
+            {
+                stringBuilder.Append('.');
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    public static ActionResult<T> ToActionResult<T>(this Result<T> result, ControllerBase controller)
+    {
+        return controller.ToActionResult(result);
+    }
+
+    public static ActionResult<object> ToActionResult(this Result result, ControllerBase controller)
+    {
+        return controller.ToActionResult(result);
+    }
+}
